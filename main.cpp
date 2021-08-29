c524fe0f256c8172ba4ef03afa56c71261b6e3be
Manaswini1832 <munugurimanaswini@gmail.com>
2021-08-29 21:24:08 +0530

    Learnt how to take string input which contains spaces

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+//#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    string name;
+
+    cout << "Enter your age : ";
+    getline(cin, name);
+
+    cout << "You are " << name << endl;
+
+    return 0;
+}
