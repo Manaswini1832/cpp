0e3910c584fafa84755996f2b25570a182bdb18b
Manaswini <58681405+Manaswini1832@users.noreply.github.com>
2021-09-02 18:54:01 +0530

    switch-case

--- main.cpp
+++ main.cpp
@@ -2,18 +2,45 @@
 
 using namespace std;
 
+string getDay(int num){
+    string day;
+    switch(num){
+    case 1 :
+        day = "Sunday";
+        break;
+    case 2 :
+        day = "Monday";
+        break;
+    case 3 :
+        day = "Tuesday";
+        break;
+    case 4 :
+        day = "Wednesday";
+        break;
+    case 5 :
+        day = "Thursday";
+        break;
+    case 6 :
+        day = "Friday";
+        break;
+    case 7 :
+        day = "Saturday";
+        break;
+    default :
+        day = "Invalid number entered";
+        break;
+    }
+
+    return day;
+}
+
 int main()
 {
-    int num1 = 4;
-    int num2 = 4;
-
-    if(num1 > num2){
-        cout << num1 << " is greater than " << num2 << endl;
-    }else if(num1 < num2){
-        cout << num1 << " is lesser than " << num2 << endl;
-    }else{
-        cout << num1 << " is equal to " << num2 << endl;
-    }
+    int num;
+    cout << "Enter a number corresponding to a weekday" << endl;
+    cin >> num;
+    string result = getDay(num);
+    cout << result << endl;
 
     return 0;
 }
