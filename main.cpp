3a4dbedf1b21f6316378a1a116a881ec5c855d17
Manaswini <58681405+Manaswini1832@users.noreply.github.com>
2021-09-02 18:48:01 +0530

    If-else statements

--- main.cpp
+++ main.cpp
@@ -2,24 +2,18 @@
 
 using namespace std;
 
-//Could do the following or we could instead also define the function on top of the main() function block itself
-
-//Function stub
-void sayHi(string name);
-
 int main()
 {
-    string name;
-    cout << "Enter your name : " << endl;
-    getline(cin, name);
+    int num1 = 4;
+    int num2 = 4;
 
-    // Function call
-    sayHi(name);
+    if(num1 > num2){
+        cout << num1 << " is greater than " << num2 << endl;
+    }else if(num1 < num2){
+        cout << num1 << " is lesser than " << num2 << endl;
+    }else{
+        cout << num1 << " is equal to " << num2 << endl;
+    }
 
     return 0;
-}
-
-//Function definition
-void sayHi(string name){
-    cout << "Hey " << name << endl;
 }
