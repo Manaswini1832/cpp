cc330457cff8d65307678af42ff8a9a34335b1a5
Manaswini <58681405+Manaswini1832@users.noreply.github.com>
2021-09-02 19:05:27 +0530

    for

--- main.cpp
+++ main.cpp
@@ -4,25 +4,11 @@ using namespace std;
 
 int main()
 {
-    int index = 6;
+    int index = 0;
 
-    //Checks condition and only then executes the code
-    //Won't print 6 to the console
-    while(index <= 5){
-        cout << "This is the while loop" << endl;
+    for(index; index <= 5; index++){
         cout << index << endl;
-        index ++;
     }
-
-    index = 6;
-
-    //Executes code and checks for conditions
-    //So it will print out 6 to the console
-    do{
-       cout << "This is the do while loop" << endl;
-       cout << index << endl;
-       index++;
-    }while(index <= 5);
 
     return 0;
 }
