0e3619573f5a64f83629dc85830a0dffb445771a
Manaswini <58681405+Manaswini1832@users.noreply.github.com>
2021-09-02 19:11:58 +0530

    2d arrays and nested for loops

--- main.cpp
+++ main.cpp
@@ -4,10 +4,17 @@ using namespace std;
 
 int main()
 {
-    int index = 0;
+    int arr2d[3][2] = {
+    {1,2},
+    {3,4},
+    {5,6}
+    };
 
-    for(index; index <= 5; index++){
-        cout << index << endl;
+    for(int rowInd = 0; rowInd < 3; rowInd++){
+        for(int colInd = 0; colInd < 2; colInd++){
+            cout << arr2d[rowInd][colInd] << " ";
+        }
+        cout << endl;
     }
 
     return 0;
