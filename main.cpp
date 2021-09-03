5786bde4b50b019e7b33c550e7aada2b351cd976
Manaswini <58681405+Manaswini1832@users.noreply.github.com>
2021-09-03 18:27:34 +0530

    getter and setter functions

--- main.cpp
+++ main.cpp
@@ -2,37 +2,51 @@
 
 using namespace std;
 
+// Aim or the motivation behind using getters and setters
+// Say we want the rating to be restricted to an int between 0 and 5 and we want to set the rating to 0 if not rated
+// We can do that by setting the rating variable inside the Book class to be private
+// Now that we can't access this private variable outside the class, to get it or to set it to another value we need to create functions
+// The getter function is getRating()
+// The setter function is setRating()
+
 class Book{
+    private :
+        int rating;
     public :
         string title;
         string author;
         string publisher;
-        int rating;
 
     // Constructor function
     Book(string aTitle, string aAuthor, string aPublisher, int aRating){
         title = aTitle;
         author = aAuthor;
         publisher = aPublisher;
-        rating = aRating;
+
+        // Whenever rating is entered as a parameter to the object, the setRating function needs to be called
+        setRating(aRating);
     }
 
-    // Object functions or also called instance functions
-    bool isFavorite(){
-            if(rating == 5){
-                return true;
+    // Setter function : setRating()
+    void setRating(int userEnteredRating){
+            if(userEnteredRating < 0 || userEnteredRating > 5){
+                rating = 0;
+            }else{
+                rating = userEnteredRating;
             }
-            return false;
+    }
+
+    // Getter function : getRating()
+    int getRating(){
+        return rating;
     }
 
 };
 
 int main()
 {
-    Book book1("Anne of Green gables", "L.M.Montgomery", "Bantam books", 5);
-    Book book2("Anne of Windy poplars", "L.M.Montgomery", "Bantam books", 4.5);
-
-    cout << book1.isFavorite() << endl; // Anne of Green gables
-    cout << book2.isFavorite() << endl; // No author
+    Book book1("Anne of Green gables", "L.M.Montgomery", "Bantam books", 4);
+    book1.setRating(5);
+    cout << book1.getRating() << endl;
     return 0;
 }
