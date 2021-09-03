c2cb2cf186f2a3aadd01016785278ca1d1bfaf84
Manaswini <58681405+Manaswini1832@users.noreply.github.com>
2021-09-03 18:06:26 +0530

    constructor functions

--- main.cpp
+++ main.cpp
@@ -9,18 +9,30 @@ class Book{
         string title;
         string author;
         string publisher;
+
+    // This is a constructor function
+    // It makes it easier to create objects by just passing parameters to the instance
+    Book(string aTitle, string aAuthor, string aPublisher){
+        title = aTitle;
+        author = aAuthor;
+        publisher = aPublisher;
+    }
+
+    // We could create multiple constructor functions as well
+    // For example, we can create a constructor function like the following to fill in default values if no parameters are passed to the instance
+    Book(){
+        title = "No title";
+        author = "No author";
+        publisher = "No publisher";
+    }
 };
 
 int main()
 {
-    // Following is an instance of that "Book" class
-    // This instance is called an object
-    Book book1;
+    Book book1("Anne of Green gables", "L.M.Montgomery", "Bantam books");
+    Book book2;
 
-    book1.title =  "Anne of Green gables";
-    book1.author = "L.M.Montgomery";
-    book1.publisher = "Bantam books";
-
-    cout << book1.title << endl;
+    cout << book1.title << endl; // Anne of Green gables
+    cout << book2.author << endl; // No author
     return 0;
 }
